be73a4ca6a376d078c5748b224e216261c5e2637
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-02-22 14:51:25 +0100

    [add] Different Styles

--- js/script.js
+++ js/script.js
@@ -6,12 +6,16 @@ for (i = 1; i <= 100; i++) {
 
   if (!(i % 3) && !(i % 5))  {
     element.innerHTML = 'fizzbuzz';
+    element.classList.add('fizzbuzz');
   } else if (!(i % 3)) {
     element.innerHTML = 'fizz';
+    element.classList.add('fizz');
   } else if (!(i % 5)) {
     element.innerHTML = 'buzz';
+    element.classList.add('buzz');
   } else {
     element.innerHTML = i; 
+    element.classList.add('counter-number');
   }
 
   container.append(element)
